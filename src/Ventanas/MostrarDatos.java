25f74206f6609522a434d07523d451a5b0907d79
Juan Ayala Lopez <Juan_Atzuya@hotmail.com>
2018-03-30 13:05:05 -0600

    Modificar Data Grid

--- src/Ventanas/MostrarDatos.java
+++ src/Ventanas/MostrarDatos.java
@@ -5,6 +5,8 @@
  */
 package Ventanas;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
@@ -22,13 +24,26 @@ public class MostrarDatos extends javax.swing.JFrame {
         initComponents();
         String[] columnas = {"cñora", "su", "hijo", "esta", "leyendo", "la", "Biblia"};
         DefaultTableModel modelo = new DefaultTableModel(0, 0);
+        DefaultListModel listaModelo = new DefaultListModel();
         for (int i = 0; i < columnas.length; i++) {
             modelo.addColumn(columnas[i]);
+            listaModelo.addElement(columnas[i]);
         }
+        listaAtributos.setModel(listaModelo);
         dataGrid.setModel(modelo);
         dataGrid.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         dataGrid.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         dataGrid.setAutoscrolls(true);
+        datosGenerales.append("Numero de Instancias: \n");
+        datosGenerales.append("Numero de Atributos: \n");
+        datosGenerales.append("Numero de Valores Faltantes: \n");
+        datosGenerales.append("Porcentaje de Valores Faltantes: \n");
+        modelo = (DefaultTableModel) dataGrid.getModel();
+        String[] datos = new String[6];
+        modelo.addRow(datos);
+        modelo.addRow(datos);
+        modelo.addRow(datos);
+        dataGrid.setModel(modelo);
     }
 
     /**
@@ -40,13 +55,43 @@ public class MostrarDatos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
         dataGrid = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        datosGenerales = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        botonAgregar = new javax.swing.JButton();
+        botonEliminar = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        listaAtributos = new javax.swing.JList<>();
+        botonEliminarAtributo = new javax.swing.JButton();
+        botonEditarAtributo = new javax.swing.JButton();
+        botonExpresion = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
+
+        jMenuItem2.setText("jMenuItem2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jScrollPane1.setVerifyInputWhenFocusTarget(false);
 
+        dataGrid.setAutoCreateRowSorter(true);
         dataGrid.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -60,29 +105,256 @@ public class MostrarDatos extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(dataGrid);
 
+        datosGenerales.setColumns(20);
+        datosGenerales.setRows(5);
+        jScrollPane2.setViewportView(datosGenerales);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Nombre de la Base de Datos xD");
+
+        jLabel2.setText("Datos Generales de la Base de Datos");
+
+        jLabel3.setText("Datos del Atributo");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane3.setViewportView(jTextArea1);
+
+        botonAgregar.setText("Agregar");
+        botonAgregar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botonAgregarMouseClicked(evt);
+            }
+        });
+
+        botonEliminar.setText("Eliminar");
+        botonEliminar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botonEliminarMouseClicked(evt);
+            }
+        });
+
+        listaAtributos.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane4.setViewportView(listaAtributos);
+
+        botonEliminarAtributo.setText("Eliminar");
+        botonEliminarAtributo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botonEliminarAtributoMouseClicked(evt);
+            }
+        });
+
+        botonEditarAtributo.setText("Editar");
+        botonEditarAtributo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botonEditarAtributoMouseClicked(evt);
+            }
+        });
+
+        botonExpresion.setText("Editar Expresion");
+        botonExpresion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botonExpresionMouseClicked(evt);
+            }
+        });
+
+        jMenu1.setText("Archivo");
+
+        jMenuItem4.setText("Cargar ");
+        jMenu1.add(jMenuItem4);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Guardar");
+
+        jMenuItem3.setText("Guardar");
+        jMenu2.add(jMenuItem3);
+
+        jMenuItem5.setText("Guardar Como");
+        jMenu2.add(jMenuItem5);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Acerca de");
+
+        jMenuItem7.setText("Software");
+        jMenu3.add(jMenuItem7);
+
+        jMenuItem6.setText("Salir");
+        jMenu3.add(jMenuItem6);
+
+        jMenuBar1.add(jMenu3);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 602, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 644, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(105, 105, 105)
+                        .addComponent(botonAgregar)
+                        .addGap(38, 38, 38)
+                        .addComponent(botonEliminar)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jScrollPane3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 292, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(botonEliminarAtributo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(botonEditarAtributo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(botonExpresion)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(134, 134, 134)
+                .addComponent(jLabel1)
+                .addGap(205, 205, 205)
+                .addComponent(jLabel2)
+                .addContainerGap(163, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
-                .addContainerGap(14, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(14, 14, 14))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(0, 28, Short.MAX_VALUE)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(botonAgregar)
+                            .addComponent(botonEliminar)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane3)
+                            .addComponent(jScrollPane4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(botonEliminarAtributo)
+                            .addComponent(botonEditarAtributo)
+                            .addComponent(botonExpresion))))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void botonAgregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonAgregarMouseClicked
+        DefaultTableModel modelo = new DefaultTableModel(0, 0);
+        modelo = (DefaultTableModel) dataGrid.getModel();
+        String[] datos = new String[6];
+        modelo.addRow(datos);
+        dataGrid.setModel(modelo);
+    }//GEN-LAST:event_botonAgregarMouseClicked
+
+    private void botonEliminarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonEliminarMouseClicked
+        if (dataGrid.getSelectedRow() >= 0) {
+            DefaultTableModel modelo = new DefaultTableModel(0, 0);
+            modelo = (DefaultTableModel) dataGrid.getModel();
+            modelo.removeRow(dataGrid.getSelectedRow());
+            dataGrid.setModel(modelo);
+        } else {
+            JOptionPane.showMessageDialog(null, "Es necesario seleccionar una fila primero.");
+
+        }
+    }//GEN-LAST:event_botonEliminarMouseClicked
+
+    private void botonEditarAtributoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonEditarAtributoMouseClicked
+        if (listaAtributos.getSelectedIndex() >= 0) {
+            String respuesta = "";
+            respuesta = JOptionPane.showInputDialog(null, "Introduce el nuevo Nombre:", listaAtributos.getSelectedValue());
+            if (respuesta != null && !respuesta.equals("")) {
+                DefaultListModel listaModelo = (DefaultListModel) listaAtributos.getModel();
+                listaModelo.set(listaAtributos.getSelectedIndex(), respuesta);
+                listaAtributos.setModel(listaModelo);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Es necesario seleccionar un Atributo.");
+        }
+    }//GEN-LAST:event_botonEditarAtributoMouseClicked
+
+    private void botonEliminarAtributoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonEliminarAtributoMouseClicked
+        if (listaAtributos.getSelectedIndex() >= 0) {
+            DefaultListModel listaModelo = (DefaultListModel) listaAtributos.getModel();
+            listaModelo.remove(listaAtributos.getSelectedIndex());
+            listaAtributos.setModel(listaModelo);
+        } else {
+            JOptionPane.showMessageDialog(null, "Es necesario seleccionar un Atributo.");
+        }
+    }//GEN-LAST:event_botonEliminarAtributoMouseClicked
+
+    private void botonExpresionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonExpresionMouseClicked
+        if (listaAtributos.getSelectedIndex() >= 0) {
+            String respuesta = JOptionPane.showInputDialog(null, "Actualiza la expresion:", listaAtributos.getSelectedValue());
+            if (respuesta != null && !respuesta.equals("")) {
+                
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Es necesario seleccionar un Atributo.");
+        }
+    }//GEN-LAST:event_botonExpresionMouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonAgregar;
+    private javax.swing.JButton botonEditarAtributo;
+    private javax.swing.JButton botonEliminar;
+    private javax.swing.JButton botonEliminarAtributo;
+    private javax.swing.JButton botonExpresion;
     private javax.swing.JTable dataGrid;
+    private javax.swing.JTextArea datosGenerales;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JList<String> listaAtributos;
     // End of variables declaration//GEN-END:variables
 }
