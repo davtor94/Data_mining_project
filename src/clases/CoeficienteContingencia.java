21aae0711eac22f547d31c6e0681058040e5c93c
Juan Ayala Lopez <Juan_Atzuya@hotmail.com>
2018-04-04 20:06:31 -0500

    Actualizacion

---
+++ src/clases/CoeficienteContingencia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author juan_
+ */
+public class CoeficienteContingencia {
+    String[] arregloNombres;
+    
+    CoeficienteContingencia(atributo atributo1, atributo atributo2){
+        arregloNombres=new String[atributo1.getInstancias().size()];
+        
+    }
+}
