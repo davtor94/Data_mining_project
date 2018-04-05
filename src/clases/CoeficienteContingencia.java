c99e0864c27c7ce184c4c4e3c5d896eb3598cd88
Juan Ayala Lopez <Juan_Atzuya@hotmail.com>
2018-04-04 22:19:55 -0500

    Actualizacion

--- src/clases/CoeficienteContingencia.java
+++ src/clases/CoeficienteContingencia.java
@@ -5,15 +5,39 @@
  */
 package clases;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author juan_
  */
 public class CoeficienteContingencia {
+
     String[] arregloNombres;
-    
-    CoeficienteContingencia(atributo atributo1, atributo atributo2){
-        arregloNombres=new String[atributo1.getInstancias().size()];
-        
+    ArrayList<BiFrecuencias> tablaFrecuencias;
+    atributo atributo1;
+    atributo atributo2;
+
+    CoeficienteContingencia(atributo atributo1, atributo atributo2) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+        arregloNombres = new String[2];
+        arregloNombres[0] = "yes";
+        arregloNombres[1] = "no";
+        tablaFrecuencias = new ArrayList<BiFrecuencias>();
+        tablaFrecuencias.add(new BiFrecuencias());
+        tablaFrecuencias.add(new BiFrecuencias());
+        tablaFrecuencias.add(new BiFrecuencias());
+        tablaFrecuencias.get(0).setNombre("overcast");
+        tablaFrecuencias.get(1).setNombre("rainy");
+        tablaFrecuencias.get(2).setNombre("sunny");
+    }
+
+    public void createTable() {
+        int numeroInstancias = atributo1.getInstancias().size();
+        int contador=0;
+        for (int j=0; j<numeroInstancias; j++) {
+            
+        }
     }
 }
