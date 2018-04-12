086a0a059ad4150d129101e07e1f68cc31f39f0a
Juan <Juan@Haunter>
2018-04-11 23:31:36 -0500

    Actualizacion

--- src/clases/BiFrecuencias.java
+++ src/clases/BiFrecuencias.java
@@ -14,27 +14,57 @@ import java.util.List;
  */
 public class BiFrecuencias {
 
-    private String name;
     private ArrayList<Long> listaFrecuencias;
+    private long total;
+    private ArrayList<Double> listaFrecuenciaEsperada;
+    private ArrayList<Double> listaEquisCuadrada;
 
     BiFrecuencias() {
         listaFrecuencias = new ArrayList<Long>();
-        name = "";
+        listaFrecuenciaEsperada = new ArrayList<Double>();
+        listaEquisCuadrada = new ArrayList<Double>();
+        total = 0;
+    }
+
+    public void aumentarContador(int indice) {
+        long contador = listaFrecuencias.get(indice);
+        contador++;
+        listaFrecuencias.set(indice, contador);
     }
 
     public void setListaValores(ArrayList<Long> lista) {
         listaFrecuencias = lista;
     }
 
-    public void setNombre(String name) {
-        this.name = name;
+    public void setListaFrecuencias(ArrayList<Double> lista) {
+        listaFrecuenciaEsperada = lista;
+    }
+
+    public void setListaEquisCuadrada(ArrayList<Double> lista) {
+        listaEquisCuadrada = lista;
     }
 
     public ArrayList<Long> getListaValores() {
         return listaFrecuencias;
     }
 
-    public String getName() {
-        return name;
+    public ArrayList<Double> getListaFrecuenciasEsperadas() {
+        return listaFrecuenciaEsperada;
     }
+
+    public ArrayList<Double> getListaEquisCuadrada() {
+        return listaEquisCuadrada;
+    }
+    
+    public long getTotal(){
+        return total;
+    }
+
+    public void calcularTotal() {
+        total = 0;
+        for (int i = 0; i < listaFrecuencias.size(); i++) {
+            total += listaFrecuencias.get(i);
+        }
+    }
+
 }
