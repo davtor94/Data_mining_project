76925b93a187d2633b52c8f2fec71ecc5e114fdc
Juan <Juan@Haunter>
2018-04-11 23:00:56 -0500

    Comentarios :v

--- src/clases/RenderPro.java
+++ src/clases/RenderPro.java
@@ -15,6 +15,8 @@ import javax.swing.table.DefaultTableCellRenderer;
  *
  * @author juan_
  */
+
+//Clase que se encarga de realizar el renderizado de cada celda
 public class RenderPro extends DefaultTableCellRenderer {
 
     DataSet baseDatos = new DataSet();
@@ -23,15 +25,20 @@ public class RenderPro extends DefaultTableCellRenderer {
         this.baseDatos = baseDatos;
     }
 
+    //Metodo que recoge cierta informacion para el renderizado
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        //validamos que la columna sea mayor a cero, ya que la primera es una columnna extra para identificar cada instancia
         if (column > 0) {
             String regexp = baseDatos.getAtributos().get(column - 1).getDominio();
             String cadena = value.toString();
+            //Validamos que el valor que esta en la celda sea correspondiente al dominio
             if (Pattern.matches(regexp, cadena)) {
+                //si es correcto pintamos normal
                 cell.setBackground(Color.WHITE);
                 cell.setForeground(Color.BLACK);
             } else {
+                //De otra manera lo pintamos de rojo
                 cell.setBackground(Color.RED);
                 cell.setForeground(Color.WHITE);
             }
